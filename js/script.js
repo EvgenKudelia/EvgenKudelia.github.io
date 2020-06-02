2ef19c9fdfcce68e905eb6a1e3e7928451b53e3f
EvgenKudelia <66069665+EvgenKudelia@users.noreply.github.com>
2020-06-02 22:27:57 +0300

    Добавил картинкам ховер и открытие страницы поверх другой

---
+++ js/script.js
@@ -0,0 +1,25 @@
+function open1(){
+	document.getElementById("img1").style.display = "block";
+	document.getElementById("img2").style.display = "none";
+	document.getElementById("img3").style.display = "none";
+	document.getElementById("shadow").style.display = "block";
+
+}
+function open2(){
+	document.getElementById("img1").style.display = "none";
+	document.getElementById("img2").style.display = "block";
+	document.getElementById("img3").style.display = "none";
+	document.getElementById("shadow").style.display = "block";
+}
+function open3(){
+	document.getElementById("img1").style.display = "none";
+	document.getElementById("img2").style.display = "none";
+	document.getElementById("img3").style.display = "block";
+	document.getElementById("shadow").style.display = "block";
+}
+function exit(){
+	document.getElementById("img1").style.display = "none";
+	document.getElementById("img2").style.display = "none";
+	document.getElementById("img3").style.display = "none";
+	document.getElementById("shadow").style.display = "none";
+}
