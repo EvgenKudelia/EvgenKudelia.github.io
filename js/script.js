667207e9dd454e9cfb75db967f420fb371496f93
EvgenKudelia <66069665+EvgenKudelia@users.noreply.github.com>
2020-06-02 23:47:27 +0300

    Добавил меню

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 function open1(){
 	document.getElementById("img1").style.display = "block";
 	document.getElementById("img2").style.display = "none";
+	document.getElementById("menu").style.display = "none";
 	document.getElementById("img3").style.display = "none";
 	document.getElementById("shadow").style.display = "block";
 
@@ -10,16 +11,19 @@ function open2(){
 	document.getElementById("img2").style.display = "block";
 	document.getElementById("img3").style.display = "none";
 	document.getElementById("shadow").style.display = "block";
+	document.getElementById("menu").style.display = "none";
 }
 function open3(){
 	document.getElementById("img1").style.display = "none";
 	document.getElementById("img2").style.display = "none";
 	document.getElementById("img3").style.display = "block";
 	document.getElementById("shadow").style.display = "block";
+	document.getElementById("menu").style.display = "none";
 }
 function exit(){
 	document.getElementById("img1").style.display = "none";
 	document.getElementById("img2").style.display = "none";
 	document.getElementById("img3").style.display = "none";
 	document.getElementById("shadow").style.display = "none";
+	document.getElementById("menu").style.display = "block";
 }
